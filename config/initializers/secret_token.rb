28c866e521070a7b61a453b83df31d3712f0b74e
Lars Söderkvist <updates11@mac.com>
2014-02-20 00:14:23 -0600

    Install and initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dromia::Application.config.secret_key_base = '46a41d20565b0b466b89025187d9dd09050a0778e1e49679b1cc379773a43a457b82dd6db470dc91d5188e11ff80522c427777142b79861b78373b2a49bb89c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dromia::Application.config.session_store :cookie_store, key: '_dromia_session'
