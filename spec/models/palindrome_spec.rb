5a1427df5636ada00814b19af2e9b48891525879
Lars Söderkvist <updates11@mac.com>
2014-03-08 22:42:01 -0600

    Add rspec tests for palindromes

--- spec/models/palindrome_spec.rb
+++ spec/models/palindrome_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Palindrome do
+  context "missing user" do
+    it "should not be valid" do
+      palindrome = Palindrome.new(body: "Foo")
+      # Equivalent to: refute palindrome.valid?
+      palindrome.should_not be_valid
+    end
+  end
+  context "with a user" do
+    it "should be valid" do
+      user = User.create(email: "test@email.com", password: "password", password_confirmation: "password")
+      palindrome = Palindrome.new(body: "Foo", user: user)
+      # Equivalent to: assert palindrome.valid?
+      palindrome.should be_valid
+    end
+  end
 end
